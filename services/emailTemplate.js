9d3aa8d7a85ee2e41e943469c5963d52a2eecb28
Kunal Bisht <bishtkunal40@gmail.com>
2022-09-11 17:03:40 +0530

    minor change

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -148,7 +148,7 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                           <tr>
                           <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                               <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Drop It</span>
-                              <br> Want to share a file? <a href="https://letshare7540.netlify.app/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Drop it</a>.
+                              <br> Want to share a file? <a href="https://dropp-it.netlify.app/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Drop it</a>.
                           </td>
                           </tr>
                           
